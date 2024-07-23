254aa9c7e5bc57467f36cf3c36b5adc6653b285f
Max <41009925+MAKblC@users.noreply.github.com>
2024-07-23 11:02:44 +0300

    Update Svetofor.ino

--- Codes/Svetofor.ino
+++ Codes/Svetofor.ino
@@ -368,8 +368,9 @@ bool setBusChannel(uint8_t i2c_channel) {
     return false;
   } else {
     Wire.beginTransmission(I2C_HUB_ADDR);
-    Wire.write(i2c_channel | EN_MASK);  // для микросхемы PCA9547
-    //Wire.write(0x01 << i2c_channel); // Для микросхемы PW548A
+    // для микросхемы PCA9547
+    // Wire.write(i2c_channel | EN_MASK);  
+    Wire.write(0x01 << i2c_channel); // Для микросхемы PW548A
     Wire.endTransmission();
     return true;
   }
